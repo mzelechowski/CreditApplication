fe156e28a4463193fb4baa478d72140672309747
maciek zelek <mzelechowski79@gmail.com>
2020-11-03 18:23:57 +0100

    Initial Commit

---
+++ src/pl/creditapp/core/model/Education.java
@@ -0,0 +1,9 @@
+package pl.creditapp.core.model;
+
+public enum Education {
+    PRIMARY,
+    MIDDLE,
+    SECONDARY,
+    POST_SECONDARY,
+    TERTIARY
+}
---
+++ src/pl/creditapp/core/model/MaritalStatus.java
@@ -0,0 +1,9 @@
+package pl.creditapp.core.model;
+
+public enum MaritalStatus {
+    SINGLE,
+    MARRIED,
+    DIVORCED,
+    SEPARATED,
+    WIDOWED
+}
